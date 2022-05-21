5764ee6df4d46040af9d6bf9a20b51a018339fcd
SERGIO OCTAVIO MANCEBO <79695876+sergio-octavio@users.noreply.github.com>
2022-05-21 22:43:49 +0200

    Add tests

---
+++ MP_Grupo_M/Test.java
@@ -0,0 +1,14 @@
+package mp_grupo_m;
+
+import org.junit.jupiter.api .*;
+import static org.junit.jupiter.api.Assertions .*;
+import jdk.jfr.StackTrace;
+
+public class Test {
+    @StackTrace
+
+
+    public class claseQueQuieroProbarTest {
+    }
+
+}
---
+++ MP_Grupo_M/TestSuit.java
@@ -0,0 +1,17 @@
+package mp_grupo_m;
+
+import org.junit.runner.RunWith;
+import org.junit.runner.Runner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({SumaTest.class})
+
+class TestSuit {
+
+
+
+
+
+}
