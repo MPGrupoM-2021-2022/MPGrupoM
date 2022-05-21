c62a4d254c7e7f998c6332031b8bc237cdc340cf
SERGIO OCTAVIO MANCEBO <79695876+sergio-octavio@users.noreply.github.com>
2022-05-21 23:00:12 +0200

    Add files via upload

--- MP_Grupo_M/src/mp_grupo_m/Tests/Test.java
+++ MP_Grupo_M/src/mp_grupo_m/Tests/Test.java
@@ -2,7 +2,9 @@ package mp_grupo_m.Tests;
 
 import junit.framework.TestCase
 import mp_grupo_m.Entidades.Cliente;
+import mp_grupo_m.Entidades.Combate;
 import mp_grupo_m.Entidades.Personaje;
+import mp_grupo_m.Ficheros.LecturaFicheroCombate;
 import mp_grupo_m.Ficheros.LecturaFicheroOperadores;
 import mp_grupo_m.Ficheros.LecturaFicheroUsuarios;
 
@@ -17,18 +19,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class Test {
-    @Test
-    //comprobar la cantidad de personajes
-    public class TestSuma extends TestCase {
-        LecturaFicheroUsuarios lecturaFicheroOperadores = new LecturaFicheroUsuarios();
-        List<Cliente> listaClientes = lecturaFicheroOperadores.lecturaFicheroUsuarios();
-        List<String> lista = new ArrayList<>();
-        count = 0;
-        for (int i = 0; i<listaCliente.size, i++){
-            count++;
-            return count;
 
+    public class TestSuma extends TestCase {
+
+        //Comprobar clientes exixtentes
+        @Test
+        public void testComprobarClientes() {
+            LecturaFicheroUsuarios lecturaFicheroOperadores = new LecturaFicheroUsuarios();
+            List<Cliente> listaClientes = lecturaFicheroOperadores.lecturaFicheroUsuarios();
+            int count = 0;
+            for (int i = 0; i < listaClientes.size(); i++){
+                count++;
+            }
+            return listaClientes;
         }
 
+        @Test
+        public void testComprobarCombates() {
+            LecturaFicheroCombate lecturaFicheroCombate = new LecturaFicheroCombate();
+            List<Combate> listaCombates = lecturaFicheroCombate.lecturaFicheroCombate();
+            int count = 0;
+            for (int i = 0; i < listaCombates.size(); i++){
+                count++;
+            }
+            return listaCombates;
+        }
     }
 }
